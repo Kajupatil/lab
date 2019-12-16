fc143b31987d5796f963142c965adebe657f93a5
Kajupatil <52774570+Kajupatil@users.noreply.github.com>
2019-12-16 16:08:33 +0530

    Add files via upload

---
+++ Tic_Tac_Toe_Game.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwarearch;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Tic_Tac_Toe_Game {
+      
+	public static void main(String[] args) {
+		
+		TicTacToe t = new TicTacToe();
+		Scanner s = new Scanner(System.in);
+		int x=0,y=0;
+		do
+		{
+			System.out.println(t.player==TicTacToe.X?"Player X turn":"Player O turn");
+			System.out.println("Enter x and y places");
+			x=s.nextInt();
+			y=s.nextInt();
+			
+			t.putSign(x, y);
+			System.out.println(t.toString());
+			System.out.println("_____________________________");
+			t.displayWinner();
+			
+		}while(t.isEmpty);
+	}
+}
+
+class TicTacToe
+{
+	public static final int X = 1, O = -1;
+	public static final int EMPTY = 0;
+	
+	public int player = X;
+	private int[][] board = new int[3][3];
+	public boolean isEmpty = false;
+	
+	public void putSign(int x, int y)
+	{
+		if(x<0 || x>2 || y<0 || y>2)
+		{
+			System.out.println("Invalid board position");
+			return;
+		}
+		if(board[x][y] != EMPTY)
+		{
+			System.out.println("Board position occupied");
+			return;
+		}
+		board[x][y] = player;
+		player = -player;
+	}
+	
+	public boolean isWin(int player)
+	{
+		return ((board[0][0] + board[0][1] + board[0][2] == player*3) ||
+			(board[1][0] + board[1][1] + board[1][2] == player*3) ||
+			(board[2][0] + board[2][1] + board[2][2] == player*3));
+			
+	}
+	
+	public void displayWinner()
+	{
+		if(isWin(X))
+		{
+			System.out.println("\n X wins...!!");
+			isEmpty=false;
+		}
+		else if(isWin(O))
+		{
+			System.out.println("\n O wins...!!");
+			isEmpty=false;
+		}
+		else
+		{
+			if(!isEmpty)
+			{
+				System.out.println("...Draw!!!...");
+			}
+			
+		}
+	}
+	
+	public String toString()
+	{
+		StringBuilder s = new StringBuilder();
+		isEmpty = false;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				switch(board[i][j])
+				{
+				case X: 
+					s.append(" X ");
+					break;
+				case O: 
+					s.append(" O ");
+					break;
+				case EMPTY: 
+					s.append("   ");
+					isEmpty=true;
+					break;
+				}
+				if(j<2)
+				{
+					s.append("|");
+				}
+				
+			}
+			if(i<2)
+			{
+				s.append("\n-----------\n");
+			}
+		}
+		return s.toString();
+	}
+}
+    
+
